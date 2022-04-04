7c9215127920eca82f1ee5d31a785bb1b57453e3
Sarath13-dev <ramsssjrs@gmail.com>
2022-04-04 11:45:33 +0530

    perf

--- src/org/individuallogin/Employee.java
+++ src/org/individuallogin/Employee.java
@@ -2,5 +2,7 @@ package org.individuallogin;
 public class Employee {
 	public static void main(String[] args) {
 		NewAccount a = new NewAccount();
+		System.out.println("work done by perf");
+		System.out.println("code is running fine in the local machine");
 	}
 }
